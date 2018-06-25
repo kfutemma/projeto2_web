addb8eaa8626b53334311345fe620c69a442b0c4
Kaique Futemma <kaiquefutemma@Kaiques-MacBook.local>
2018-06-24 22:49:38 -0300

    finalizacao do metodo de edicao na lista

--- projeto2_web/JavaScript/telaListar.js
+++ projeto2_web/JavaScript/telaListar.js
@@ -2,11 +2,6 @@
 
 $(document).ready(function(){
 	carregarCandidatos();
-
-
-
-
-
 });
 
 function carregarCandidatos(){
@@ -18,16 +13,18 @@ function carregarCandidatos(){
 
         var id= index;
 
-        var excluirCandidato = '<button onClick="excluirCandidato('+data[index].idcandidato+')" class="btn btn-danger">Remover</button>';
+        var excluirCandidato = '<button onClick="excluirCandidato('+data[index].idcandidato+')" class="btn btn-danger"> <span class="glyphicon glyphicon-trash"></span></button>';
 
-        var editarCandidato = '<button onClick="editarCandidato('+id+')" class="btn btn-warning openmodal">Editar</button>';
+        var editarCandidato = '<button onClick="editarCandidato('+id+')" class="btn btn-default openmodal"><span class="glyphicon glyphicon-pencil"></span></button>';
 
         console.log(editarCandidato);
         
         tabelaDados += '<tr>' +
-                              '<td>'+ data[index].idcandidato + '</td>' + '<td id=nome'+id+'>'+ data[index].nome + '</td>' + '<td id=sexo'+id+'>'+ data[index].sexo + 
-                              '</td>' + '<td id=cidade'+id+'>'+ data[index].cidade + '</td>'+ '<td id=estado'+id+'>'+ data[index].estado + '</td>' + '<td id=email'+id+'>'+ data[index].email +
-                              '</td>' + '<td id=cpf'+id+'>'+ data[index].cpf + '</td>' + '<td>'+ excluirCandidato + '</td>' + '<td>'+ editarCandidato + '</td>'+
+                              '<td id=id'+id+'>'+ data[index].idcandidato + '</td>' + '<td id=nome'+id+'>'+ data[index].nome + '</td>' + '<td id=sexo'+id+'>'+ data[index].sexo + 
+                              '</td>' + '<td id=cidade'+id+'>'+ data[index].cidade + '</td>'+ '<td id=estado'+id+'>'+ data[index].estado + '</td>' + 
+                              '<td id=rua'+id+'>'+ data[index].rua + '</td>' + '<td id=numero'+id+'>'+ data[index].numero + '</td>' + '<td id=email'+id+'>'+ data[index].email +
+                              '</td>' + '<td id=cpf'+id+'>'+ data[index].cpf + '</td>' + '<td id=cadjus'+id+'>'+ data[index].cadjus + '</td>' + '<td id=dataNasc'+id+'>'+ data[index].datanasc + '</td>'+ 
+                              '<td>'+ editarCandidato + '</td>' + '<td>'+ excluirCandidato + '</td>'+
                        '</tr>';                       
         
       }
@@ -66,46 +63,49 @@ function editarCandidato(id){
 
     $('#nome').val($('#nome'+id).text());
     $('#sexo').val($('#sexo'+id).text());
-    $('#dataNasc').val($('#dataNasc'+id).val());
-    $('#rua'+id).val();
-    $('#numero'+id).val();
-    $('#estadoID option:selected').text();
-    $('#cidadeID option:selected').text();
-    $('#cpf'+id).val();
-    $('#cadjus'+id).val();
-    $('#email'+id).val();
+    $('#dataNasc').val($('#dataNasc'+id).text());
+    $('#rua').val($('#rua'+id).text());
+    $('#numero').val($('#numero'+id).text());
+    $('#estado').val($('#estado'+id).text());
+    $('#cidade').val($('#cidade'+id).text());
+    $('#cpf').val($('#cpf'+id).text());
+    $('#cadjus').val($('#cadjus'+id).text());
+    $('#email').val($('#email'+id).text());
     $('#senha').val();
 
     
     $("#myModal").modal("show");
-    /*
-    var nome = $('#nome').val();
-    var sexo = $('#sexo').val();
-    var dataNasc = $('#dataNasc').val();
-    var rua = $('#rua').val();
-    var numero = $('#numero').val();
-    var bairro = $('#bairro').val();
-    var estado = $('#estadoID option:selected').text();
-    var cidade = $('#cidadeID option:selected').text();
-    var cpf = $('#cpf').val();
-    var cadjus = $('#cadjus').val();
-    var email = $('#email').val();
-    var senha = $('#senha').val();
+    
+    
 
     
-    
-    $.ajax({
-            url: 'http://andrebordignon.esy.es/php/atualizacandidato.php',
-              success:function(data){
-                alert(data);
-                  console.log(data);
-                  location.reload();
-              },
-              error: function(data){
-                console.log(data);
-                alert(data);
-              }   
-          });
-          */
+    $('#salvar').click(function() {
+        var idcandidato = $('#id'+id).text();
+        var nome = $('#nome').val();
+        var sexo = $('#sexo').val();
+        var dataNasc = $('#dataNasc').val();
+        var rua = $('#rua').val();
+        var numero = $('#numero').val();
+        var estado = $('#estadoID option:selected').text();
+        var cidade = $('#cidadeID option:selected').text();
+        var cpf = $('#cpf').val();
+        var cadjus = $('#cadjus').val();
+        var email = $('#email').val();
+
+        $.ajax({
+          type: 'POST',
+          url:'http://andrebordignon.esy.es/php/atualizacandidato.php',
+          data:{idcandidato:idcandidato,nome:nome, sexo:sexo,dataNasc:dataNasc,rua:rua,
+                numero:numero,estado:estado, cidade:cidade, cpf:cpf, cadjus:cadjus,
+                email:email},
+          success:function(data, status, jqXHR){
+              alert(data);
+              console.log(data);
+              location.reload();
+          },
+          error: function(data){
+              console.log(data);
+          } 
+      });
+    });
 }
-
